c7e533216a635a9d3a826e8851651d7787f4f4df
jwarzocha <warjed@gmail.com>
2018-05-13 20:15:35 +0200

    Update repset.sh

--- repset.sh
+++ repset.sh
@@ -12,13 +12,13 @@ host3='localhost'
 	mongod --replSet rs0 --port 27017 --bind_ip $host1 --dbpath /tmp/jwarzocha/carbon/data-1 --smallfiles --oplogSize 128 > /dev/null &
 	mongod --replSet rs0 --port 27018 --bind_ip $host2 --dbpath /tmp/jwarzocha/carbon/data-2 --smallfiles --oplogSize 128 > /dev/null &
 	mongod --replSet rs0 --port 27019 --bind_ip $host3 --dbpath /tmp/jwarzocha/carbon/data-3 --smallfiles --oplogSize 128 > /dev/null &
-sleep 5
+sleep 10
 	mongo --host $host1:27017 --shell ./repl_set_init.js > /dev/null &
-sleep 5
+sleep 10
 	start_time=`date +%s`
 	
-	#/usr/bin/time ../../mongoimport --host rs0/$host1:27017,$host2:27018,$host3:27019 --db test --type csv  --collection londoncrimes1 --drop --file ../dane/london_crime_m.csv --headerline 
-	/usr/bin/time mongoimport --host rs0/$host1:27017,$host2:27018,$host3:27019 --db test --type csv  --collection londoncrimes1 --drop --file ./london_crime_test.csv --headerline 
+	#time ../../mongoimport --host rs0/$host1:27017,$host2:27018,$host3:27019 --db test --type csv  --collection londoncrimes1 --drop --file ../dane/london_crime_m.csv --headerline 
+	time mongoimport --host rs0/$host1:27017,$host2:27018,$host3:27019 --db test --type csv  --collection londoncrimes1 --drop --file ./london_crime_test.csv --headerline 
 
 	end_time=`date +%s`
 	echo execution time was `expr $end_time - $start_time` s.
